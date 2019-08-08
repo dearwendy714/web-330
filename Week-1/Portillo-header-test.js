369e57786f69df9860b8c7a048f3bedd44563ed2
dearwendy <dearwendy714@gmail.com>
2019-08-08 12:58:12 -0700

    Complete assignment

---
+++ Week-1/Portillo-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  Portillo-header-test.js
+; Author: Wendy Portillo
+; Date:   8 August 2019
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../header.js');
+
+console.log(header.display("Wendy", "Portillo", "Week 1"));
